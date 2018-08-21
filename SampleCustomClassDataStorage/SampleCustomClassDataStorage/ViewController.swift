8fe1982af8758cfee41d709d150ab884dfc3d138
田﨑貴大 <qxgxyea_h@yahoo.co.jp>
2018-08-21 14:51:42 +0900

    シリアライズして独自クラスを格納

--- SampleCustomClassDataStorage/SampleCustomClassDataStorage/ViewController.swift
+++ SampleCustomClassDataStorage/SampleCustomClassDataStorage/ViewController.swift
@@ -18,8 +18,20 @@ class ViewController: UIViewController {
         let userDefaults = UserDefaults.standard
         let data = MyData()
         data.valueString = "test"
-        userDefaults.set(data, forKey: "data")
+
+        // シリアライズ処理
+        let archiveData = NSKeyedArchiver.archivedData(withRootObject: data)
+        userDefaults.set(archiveData, forKey: "data")
         userDefaults.synchronize()
+
+        // デシリアライズ処理
+        if let storedData = userDefaults.object(forKey: "data") as? Data {
+            if let unarchivedData = NSKeyedUnarchiver.unarchiveObject(with: storedData) as? MyData {
+                if let valuseString = unarchivedData.valueString {
+                    print("デシリアライズデータ：" + valuseString)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
