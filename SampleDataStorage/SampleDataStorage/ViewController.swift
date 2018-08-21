9b10f5a716d97739033ddc6de0015273bab8d7e2
田﨑貴大 <qxgxyea_h@yahoo.co.jp>
2018-08-21 10:50:49 +0900

    入力文字保存機能の実装

--- SampleDataStorage/SampleDataStorage/ViewController.swift
+++ SampleDataStorage/SampleDataStorage/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // UserDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        
+        // textというキーを指定して保存していた値を取り出す
+        if let value = userDefaults.string(forKey: "text") {
+            // 取り出した値をテキストフィールドに設定
+            textField.text = value
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // ボタンをタップした時の処理
+    @IBAction func tapActionButton(_ sender: Any) {
+        // UserDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        // textというキーで値を保存する
+        userDefaults.set(textField.text, forKey: "text")
+        // UserDefaultsへの値の保存を明示的に行う
+        userDefaults.synchronize()
+    }
 }
 
